c5962333e47fb59b7dc39325277d4bf4069be545
yingbbang <yingbbang@naver.com>
2025-12-26 17:31:25 +0900

    feat(match3): update Match-3 game screen implementation

--- bnkproject/lib/game/3match_game_screen.dart
+++ bnkproject/lib/game/3match_game_screen.dart
@@ -6,12 +6,10 @@ class Match3GameScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Image.asset(
-            'assets/game/match3_finance2.png',
-            fit: BoxFit.contain, // ✅ 전체 보이게, 잘림 없음
-          ),
+      body: SizedBox.expand(
+        child: Image.asset(
+          'assets/game/match3_finance.gif',
+          fit: BoxFit.cover,
         ),
       ),
     );
