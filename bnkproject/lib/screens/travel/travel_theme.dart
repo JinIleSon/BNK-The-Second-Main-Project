f3a415809710b0919716f0e3bb93ee962dd1e777
yingbbang <yingbbang@naver.com>
2025-12-29 19:19:00 +0900

    feat(travel): add travel theme constants

---
+++ bnkproject/lib/screens/travel/travel_theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:latlong2/latlong.dart';
+
+class TravelTheme {
+  static const boogiMint = Color(0xFF38E1C6);
+  static const boogiGold = Color(0xFFFFC93C);
+
+  static const bgTop = Color(0xFF0B1020);
+  static const bgBottom = Color(0xFF0F1730);
+
+  static const busanCenter = LatLng(35.1796, 129.0756);
+}
