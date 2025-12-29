d921e8272f94dea0c921b00959ad8210b5dcb5e1
yingbbang <yingbbang@naver.com>
2025-12-29 19:19:22 +0900

    feat(travel-map): add travel map view

---
+++ bnkproject/lib/screens/travel/map/travel_map_view.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+
+import 'place_marker_layer.dart';
+import 'spot.dart';
+
+class TravelMapView extends StatelessWidget {
+  final MapController controller;
+  final LatLng initialCenter;
+  final double initialZoom;
+  final VoidCallback onMapReady;
+
+  final List<Spot> spots;
+  final void Function(Spot spot) onTapSpot;
+
+  const TravelMapView({
+    super.key,
+    required this.controller,
+    required this.initialCenter,
+    required this.initialZoom,
+    required this.onMapReady,
+    required this.spots,
+    required this.onTapSpot,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FlutterMap(
+      mapController: controller,
+      options: MapOptions(
+        initialCenter: initialCenter,
+        initialZoom: initialZoom,
+        onMapReady: onMapReady,
+      ),
+      children: [
+        TileLayer(
+          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+          userAgentPackageName: 'kr.co.bnk.bnkproject',
+        ),
+        PlaceMarkerLayer(spots: spots, onTapSpot: onTapSpot),
+      ],
+    );
+  }
+}
